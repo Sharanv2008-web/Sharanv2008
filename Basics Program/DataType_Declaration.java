662707404c2b0abd76b03f56ed376173181f0ebc
Sharan V <sharanv838@gmail.com>
2026-07-20 21:14:07 +0530

    Adding program of primitive Datatypes declaration

---
+++ Basics Program/DataType_Declaration.java
@@ -0,0 +1,18 @@
+class Sharan{
+public static void main(String[] argc){
+   System.out.println("Sharan");
+   byte a = 127;
+   short b = 32767;
+   int c = 214748;
+   long d = 9223372;
+   float e = 3.5f;
+   double f = 1.85425675;
+   System.out.println(a);
+   System.out.println(b);
+   System.out.println(c);
+   System.out.println(d);
+   System.out.println(e);
+   System.out.println(f);
+   System.out.println("JEEVA SIR IS BEST TEACHER");
+   }
+}
