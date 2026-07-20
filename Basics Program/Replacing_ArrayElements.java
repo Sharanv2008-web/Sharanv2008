a51a7b18e54bb0a60a1c556c383aa4f81f4ac3f9
Sharan V <sharanv838@gmail.com>
2026-07-20 21:05:27 +0530

    Replacing the elements in array

---
+++ Basics Program/Replacing_ArrayElements.java
@@ -0,0 +1,20 @@
+class Array{
+	public static void main(String[] argc){
+		int Myarray[] = {99,77,66,33,21};
+		System.out.println("The elements in the MyArray is:");
+		for(int i=0;i<5;i++){
+		System.out.print(Myarray[i]+" ");
+	}
+		int Myarray1[]=new int[5];
+		Myarray1[0]=11;
+		Myarray1[1]=22;
+		Myarray1[2]=33;
+		Myarray1[3]=44;
+		Myarray1[4]=55;
+		System.out.println(" ");
+		System.out.println("The elements in the MyArray1 is:");
+		for(int i=0;i<5;i++){
+		System.out.print(Myarray1[i]+" ");
+	}
+	}
+}
